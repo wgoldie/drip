75a7ad008a15b6b281c64f96f23f7a6662228bc0
Will Goldie <wgoldie@gmail.com>
2021-10-17 13:43:23 -0400

    ast

---
+++ drip_ast.py
@@ -0,0 +1,76 @@
+from dataclasses import dataclass
+import typing
+import enum
+
+@dataclass(frozen=True)
+class LiteralExpression:
+    value: float
+
+@dataclass(frozen=True)
+class VariableReferenceExpression:
+    name: str
+
+@dataclass(frozen=True)
+class ConstructionExpression:
+    type_name: str
+    arguments: typing.Dict[str, 'Expression']
+
+@dataclass(frozen=True)
+class FunctionCallExpression:
+    function_name: str
+    arguments: typing.Dict[str, 'Expression']
+
+@dataclass(frozen=True)
+class PropertyAccessExpression:
+    entity: 'Expression'
+    property_name: str
+
+class BinaryOperator(enum.Enum):
+    ADD = enum.auto()
+
+@dataclass(frozen=True)
+class BinaryOperatorExpression:
+    operator: BinaryOperator
+    lhs: 'Expression'
+    rhs: 'Expression'
+
+Expression = typing.Union[
+    ConstructionExpression,
+    VariableReferenceExpression,
+    LiteralExpression,
+    PropertyAccessExpression,
+    BinaryOperatorExpression]
+
+@dataclass(frozen=True)
+class ReturnStatement:
+    value: 'Expression'
+
+@dataclass(frozen=True)
+class AssignmentStatement:
+    variable_name: str
+    expression: Expression
+
+Statement = typing.Union[AssignmentStatement, ReturnStatement]
+
+
+
+@dataclass(frozen=True)
+class FunctionDefinition:
+    name: str
+    arguments: typing.Dict[str, str]
+    procedure: typing.Tuple[Statement, ...]
+
+@dataclass(frozen=True)
+class StructureFieldDefinition:
+    name: str
+    type_name: str
+
+@dataclass(frozen=True)
+class StructureDefinition:
+    name: str
+    fields: typing.Tuple[StructureFieldDefinition, ...]
+
+
+@dataclass(frozen=True)
+class Program:
+    bits: typing.Tuple[typing.Union[StructureDefinition, FunctionDefinition], ...]
--- tests.py
+++ tests.py
@@ -4,6 +4,7 @@ from basetypes import TaggedValue
 from interpreter import interpret_subroutine, interpret_program
 from parse import parse_asm_snippet, parse_asm_program
 from program import Program, Subroutine
+import drip_ast
 
 
 def test_ops():
@@ -144,7 +145,124 @@ def test_asm():
     END_SUBROUTINE main
     '''))
 
+def test_ast():
+    print('drip_ast')
+    drip_ast_a = drip_ast.Program(
+        bits=(
+            drip_ast.StructureDefinition(
+                name='Point',
+                fields=(
+                    drip_ast.StructureFieldDefinition(name='x', type_name='float'),
+                    drip_ast.StructureFieldDefinition(name='y', type_name='float'),
+                ),
+            ),
+            drip_ast.StructureDefinition(
+                name='Line',
+                fields=(
+                    drip_ast.StructureFieldDefinition(name='start', type_name='Point'),
+                    drip_ast.StructureFieldDefinition(name='end', type_name='Point'),
+                ),
+            ),
+            drip_ast.FunctionDefinition(
+                name='manhattan_length',
+                arguments={'line': 'Line'},
+                procedure=(
+                    drip_ast.ReturnStatement(
+                        value=drip_ast.BinaryOperatorExpression(
+                            operator=drip_ast.BinaryOperator.ADD,
+                            lhs=drip_ast.BinaryOperatorExpression(
+                                operator=drip_ast.BinaryOperator.ADD,
+                                lhs=drip_ast.PropertyAccessExpression(
+                                    entity=drip_ast.PropertyAccessExpression(
+                                        entity=drip_ast.VariableReferenceExpression(name='line'),
+                                        property_name='start',
+                                    ),
+                                    property_name='x',
+                                ),
+                                rhs=drip_ast.PropertyAccessExpression(
+                                    entity=drip_ast.PropertyAccessExpression(
+                                        entity=drip_ast.VariableReferenceExpression(name='line'),
+                                        property_name='end',
+                                    ),
+                                    property_name='x',
+                                ),
+                            ),
+                            rhs=drip_ast.BinaryOperatorExpression(
+                                operator=drip_ast.BinaryOperator.ADD,
+                                lhs=drip_ast.PropertyAccessExpression(
+                                    entity=drip_ast.PropertyAccessExpression(
+                                        entity=drip_ast.VariableReferenceExpression(name='line'),
+                                        property_name='start',
+                                    ),
+                                    property_name='y',
+                                ),
+                                rhs=drip_ast.PropertyAccessExpression(
+                                    entity=drip_ast.PropertyAccessExpression(
+                                        entity=drip_ast.VariableReferenceExpression(name='line'),
+                                        property_name='end',
+                                    ),
+                                    property_name='y',
+                                ),
+                            ),
+                        ),
+                    ),
+                )
+            ),
+            drip_ast.FunctionDefinition(
+                name='main',
+                arguments={},
+                procedure=(
+                    drip_ast.AssignmentStatement(
+                        variable_name='origin',
+                        expression=drip_ast.ConstructionExpression(
+                            type_name='Point',
+                            arguments={
+                                'x': drip_ast.LiteralExpression(value=0.),
+                                'x': drip_ast.LiteralExpression(value=0.),
+                            }
+                        )
+                    ),
+                    drip_ast.AssignmentStatement(
+                        variable_name='one_one',
+                        expression=drip_ast.ConstructionExpression(
+                            type_name='Point',
+                            arguments={
+                                'x': drip_ast.LiteralExpression(value=0.),
+                                'x': drip_ast.LiteralExpression(value=0.),
+                            }
+                        )
+                    ),
+                    drip_ast.AssignmentStatement(
+                        variable_name='line_a',
+                        expression=drip_ast.ConstructionExpression(
+                            type_name='Line',
+                            arguments={
+                                'start': drip_ast.VariableReferenceExpression(name='origin'),
+                                'end': drip_ast.VariableReferenceExpression(name='one_one'),
+                            }
+                        )
+                    ),
+                    drip_ast.AssignmentStatement(
+                        variable_name='l',
+                        expression=drip_ast.FunctionCallExpression(
+                            function_name='manhattan_length',
+                            arguments={
+                                'line': drip_ast.VariableReferenceExpression(name='line_a'),
+                            }
+                        )
+                    ),
+                    drip_ast.ReturnStatement(
+                        value=drip_ast.VariableReferenceExpression(name='l'),
+                    ),
+                )
+            )
+        )
+    )
+
+
+
 if __name__ == "__main__":
     test_ops()
     test_asm_snippets()
     test_asm()
+    test_ast()
